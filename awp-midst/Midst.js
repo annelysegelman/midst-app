74b267c56de6b800a1746a1fd644cf92da75a27a
Jason Grier <jason@hemberlin.de>
2019-03-13 15:39:13 -0500

    Basic editor functionality.

--- awp-midst/Midst.js
+++ awp-midst/Midst.js
@@ -19,15 +19,22 @@ class Midst extends React.Component {
       appDrawerOpen: false,
       appFocusMode: false,
       appTimelineMode: false,
+      editorCachedSelection: [],
       editorDraftMarkers: [],
+      editorNumLines: 0,
       editorTimelineFrames: [],
       editorTimelineIndex: 0,
+      editorTitle: 'Untitled',
     }
 
 // ================================================================================
 // Bound Methods
 // ================================================================================
-// N/A
+  this.editorOnBlur = this.editorOnBlur.bind(this)
+  this.editorOnInput = this.editorOnInput.bind(this)
+  this.editorOnPaste = this.editorOnPaste.bind(this)
+  this.toggleDrawer = this.toggleDrawer.bind(this)
+  this.toggleTimeline = this.toggleTimeline.bind(this)
 
 // ================================================================================
 // Styles
@@ -44,7 +51,35 @@ class Midst extends React.Component {
 // Lifecycle
 // ================================================================================#
   componentDidMount() {
+    $.fn.selectRange = function(start, end) {
+      return this.each(function() {
+        if (this.setSelectionRange) {
+          this.focus()
+          this.setSelectionRange(start, end)
+        } else if (this.createTextRange) {
+          var range = this.createTextRange()
+          range.collapse(true)
+          range.moveEnd('character', end)
+          range.moveStart('character', start)
+          range.select()
+        }
+      })
+    }
 
+    this.$editable = $('#editable')
+
+    this.$editable.on('keyup', (evt) => {
+      evt = evt || window.event
+      if (evt.keyCode === 13) {
+        document.execCommand('formatBlock', false, 'p')
+      }
+    })
+
+    this.$editable.html('<p><br></p>')
+
+    this.$editable.on('blur', this.editorOnBlur)
+    this.$editable.on('input', this.editorOnInput)
+    this.$editable.on('paste', this.editorOnPaste)
   }
 
   componentDidUpdate() {
@@ -52,13 +87,15 @@ class Midst extends React.Component {
   }
 
   componentWillUnmount() {
-
+    this.$editable.off('blur')
+    this.$editable.off('input')
+    this.$editable.off('paste')
   }
 
 // ================================================================================
 // Models
 // ================================================================================
-  draftMarker(data) {
+  modelDraftMarker(data) {
     return {
       author: '',
       description: '',
@@ -68,15 +105,15 @@ class Midst extends React.Component {
     }
   }
 
-  timelineFrame(data) {
+  modelTimelineFrame({ content, lineNumber, timestamp }) {
     return {
-      content: '',
-      cursor: [0, 0],
-      timestamp: 0,
+      content,
+      lineNumber,
+      timestamp,
     }
   }
 
-  midstFile(data) {
+  modelMidstFile(data) {
     return {
       timelineFrames: [],
       draftMarkers: [],
@@ -86,32 +123,120 @@ class Midst extends React.Component {
 // ================================================================================
 // Handlers
 // ================================================================================
-// N/A
+  editorOnInput() {
+    this.reflowLineNumbers()
+    this.captureTimelineFrame(this.$editable.html())
+  }
+
+  editorOnBlur() {
+    saveSelection()
+    setTimeout(() => {
+      restoreSelection()
+    }, 1)
+  }
+
+  editorOnPaste(evt) {
+    evt.preventDefault()
+
+    let content
+
+    if (window.clipboardData) {
+      content = window.clipboardData.getData('Text')
+
+      if (window.getSelection) {
+        const selObj = window.getSelection()
+        const selRange = selObj.getRangeAt(0)
+        selRange.deleteContents()
+        selRange.insertNode(document.createTextNode(content))
+      }
+    } else if (evt.originalEvent.clipboardData) {
+      content = (evt.originalEvent || evt).clipboardData.getData('text/plain')
+      document.execCommand('insertText', false, content)
+    }
+  }
+
+  toggleTimeline() {
+    this.setState({
+      appTimelineMode: !this.state.appTimelineMode,
+    }, () => {
+      if (!this.state.appTimelineMode && this.state.appDrawerOpen) {
+        this.setState({
+          appDrawerOpen: false,
+        })
+      }
+    })
+  }
+
+  toggleDrawer() {
+    this.setState({
+      appDrawerOpen: !this.state.appDrawerOpen,
+    }, () => {
+      if (this.state.appDrawerOpen) {
+        this.setState({
+          appTimelineMode: true,
+        })
+      }
+    })
+  }
 
 // ================================================================================
 // Other Methods
 // ================================================================================
-// N/A
+  reflowLineNumbers(force) {
+    const $lines = this.$editable.find('p')
+
+    if (force || (this.editorNumLines !== $lines.length)) {
+      $lines.each(function(i) {
+        $(this).attr('data-line-number', i)
+      })
+      this.editorNumLines = $lines.length
+    }
+  }
+
+  captureTimelineFrame(content) {
+    const { editorTimelineFrames } = this.state
+    const $emptyLine = $(window.getSelection().anchorNode)
+    const $emptyLineRoot = $emptyLine.parents('p')
+    const $line = $emptyLineRoot.length ? $emptyLineRoot : $emptyLine
+    const lineNumber = $line.attr('data-line-number')
+
+    const nextFrame = this.modelTimelineFrame({
+      content,
+      lineNumber,
+      timestamp: + new Date(),
+    })
+
+    this.setState({
+      editorTimelineFrames: editorTimelineFrames.concat([nextFrame])
+    })
+  }
 
 // ================================================================================
 // Render Helpers
 // ================================================================================
   renderHeader() {
+    const { editorTitle } = this.state
+
     return (
       e('div', {
-        className: 'header'
+        className: 'title-bar'
       },
-
+        e('div', { className: 'title' }, editorTitle)
       )
     )
   }
 
   renderTopToolbar() {
+    const { appFocusMode } = this.state
+
     return (
       e('div', {
         className: 'top-toolbar'
       },
-
+        e('h1', { className: 'logo' },
+          e('span', {}),
+        ),
+        e('div', { className: 'round-icon focus-mode-toggle' + (appFocusMode ? ' active' : '') }),
       )
     )
   }
@@ -122,7 +247,7 @@ class Midst extends React.Component {
         className: 'editor'
       },
         e('div', {
-          className: 'editable',
+          id: 'editable',
           contentEditable: true,
         }),
       )
@@ -130,19 +255,32 @@ class Midst extends React.Component {
   }
 
   renderBottomToolbar() {
+    const { appTimelineMode, appDrawerOpen } = this.state
+
     return (
       e('div', {
         className: 'bottom-toolbar'
       },
-
+        e('div', { className: 'double-icon timeline-toggles' },
+          e('div', {
+            className: 'icon timeline-toggle' + (appTimelineMode ? ' active' : ''),
+            onClick: this.toggleTimeline,
+          }),
+          e('div', {
+            className: 'icon drawer-toggle' + (appDrawerOpen ? ' active' : ''),
+            onClick: this.toggleDrawer,
+          }),
+        ),
       )
     )
   }
 
   renderTimeline() {
+    const { appTimelineMode, appDrawerOpen }  = this.state
+
     return (
       e('div', {
-        className: 'timeline'
+        className: 'timeline' + (appTimelineMode ? ' open' : '')
       },
 
       )
@@ -150,9 +288,11 @@ class Midst extends React.Component {
   }
 
   renderDrawer() {
+    const { appDrawerOpen }  = this.state
+
     return (
       e('div', {
-        className: 'drawer'
+        className: 'drawer' + (appDrawerOpen ? ' open' : '')
       },
 
       )
@@ -163,14 +303,16 @@ class Midst extends React.Component {
 // Render
 // ================================================================================
   render() {
+    const { appFocusMode } = this.state
+
     return (
-      e('div', { className: 'midst' },
-        e('header', { className: 'title-bar' }, this.renderHeader()),
-        e('section', { className: 'top-toolbar' }, this.renderTopToolbar()),
-        e('main', { className: 'editor' }, this.renderEditor()),
-        e('section', { className: 'bottom-toolbar' }, this.renderBottomToolbar()),
-        e('section', { className: 'timeline' }, this.renderTimeline()),
-        e('aside', { className: 'drawer' }, this.renderDrawer()),
+      e('div', { className: 'midst' + (appFocusMode ? ' focus-mode' : '') },
+        e('header', { id: 'title-bar' }, this.renderHeader()),
+        e('section', { id: 'top-toolbar' }, this.renderTopToolbar()),
+        e('main', {}, this.renderEditor()),
+        e('section', { id: 'bottom-toolbar' }, this.renderBottomToolbar()),
+        e('section', { id: 'timeline' }, this.renderTimeline()),
+        e('aside', { id: 'drawer' }, this.renderDrawer()),
       )
     )
   }
